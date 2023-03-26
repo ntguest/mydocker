f6d2092cb3a8f722283ee58c35823b08b23940c7
Andrew V. Kulikov <69485846+ntguest@users.noreply.github.com>
2023-03-26 14:35:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 error() { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
 if [[ $EUID != 0 ]]; then error This installer requires root privileges. Try again as \"root\" ... ; fi
-
+clear
 if ! whiptail -v > /dev/null 2>&1; then
   apt install whiptail -y
 fi
